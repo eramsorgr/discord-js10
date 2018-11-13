3292d9d0486cc81ec57c4b7ff73c33552221180a
Thien Tran <42406302+Ethereal739@users.noreply.github.com>
2018-11-13 04:20:59 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,7 @@ sleep 2
 
 cd /home/container
 
-# Replace Startup Variables
-MODIFIED_INSTALL=`eval echo $(echo ${INSTALL} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 
 # Run the Server
-${MODIFIED_INSTALL}
-${MODIFIED_STARTUP}
+${INSTALL}
+node ${STARTUP}
