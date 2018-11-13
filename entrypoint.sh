5434402a5102f15f80e02e69149878f7b8aecd6d
Thien Tran <42406302+Ethereal739@users.noreply.github.com>
2018-11-13 11:52:49 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 sleep 2
 
 cd /home/container
-MODIFIED_STARTUP=`eval echo $(echo npm ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
