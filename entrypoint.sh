2dd8fab5030ff946749ac0f27fa1ce5998e2b248
Thien Tran <42406302+Ethereal739@users.noreply.github.com>
2018-11-13 05:05:05 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,8 @@
 sleep 2
 
 cd /home/container
-echo "Started!"
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+${MODIFIED_STARTUP}
