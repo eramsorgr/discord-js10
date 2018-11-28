7e9699691b57e87995e6b028b2388b2bee22c7b4
Thien Tran <42406302+Ethereal739@users.noreply.github.com>
2018-11-28 06:25:51 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@ sleep 2
 
 cd /home/container
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
 npm install
